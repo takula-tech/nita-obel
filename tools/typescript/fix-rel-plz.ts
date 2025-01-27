69909e56386b59d15ff385797f7c96f915490a89
jakzha <jake.zhang@kindredgroup.com>
2025-01-27 19:52:46 +1100

    chore(skip): fix release-plz version

--- tools/typescript/fix-rel-plz.ts
+++ tools/typescript/fix-rel-plz.ts
@@ -1,7 +1,14 @@
 #!/usr/bin/env -S deno run --allow-all
 
+// to fix the issue related to release-plz with the following error message from github action output:
+// INFO  obel: the local package has already a different version with respect to the registry package,
+// so release-plz will not update it
+// this could happen when the release pr pipeline fails for some reasons, leaving the broken crate version
+// between local and remote crates.
+
 import $ from "@david/dax";
 import * as TOML from "@std/toml";
+import process from "node:process";
 
 // Define root path constant using fs api
 const rootPath = new URL("../..\n", import.meta.url).pathname;
@@ -10,13 +17,14 @@ const rootPath = new URL("../..\n", import.meta.url).pathname;
 const cargoToml = await Deno.readTextFile(`${rootPath}Cargo.toml`);
 const cargoConfig = TOML.parse(cargoToml);
 
-// Extract workspace dependencies starting with 'obel_'
-const workspaceDeps = (cargoConfig.workspace as any)?.dependencies || {};
-const depNames = Object.keys(workspaceDeps).filter((name) => name.startsWith("obel_"));
+const crates = new Map<string, { name: string; path: string; remoteVersion: string; localVersion: string }>();
 
-// Log dependency names
-console.log("All obel_* crate names:");
-depNames.forEach((name) => console.log(`- ${name}`));
+// Extract workspace dependencies starting with 'obel_'
+// deno-lint-ignore no-explicit-any
+const workspaceDeps = (cargoConfig.workspace as any)?.dependencies || {};
+Object.keys(workspaceDeps)
+  .filter((name) => name.startsWith("obel_"))
+  .forEach((v) => crates.set(v, { localVersion: workspaceDeps[v].version, path: workspaceDeps[v].path, name: v, remoteVersion: "" }));
 
 // Fetch versions from crates.io using Promise.allSettled
 console.log("\nFetching latest versions from crates.io...");
@@ -35,7 +43,7 @@ const fetchCrateVersion = async (name: string) => {
 };
 
 // Create an array of promises with delay between each request
-const fetchPromises = depNames.map(
+const fetchPromises = Array.from(crates.keys()).map(
   (name, index) =>
     new Promise((resolve) =>
       setTimeout(
@@ -48,32 +56,37 @@ const fetchPromises = depNames.map(
 // Wait for all requests to complete
 const results = await Promise.allSettled(fetchPromises);
 
-// Store results in a Map
-const crateVersions = new Map<string, { version: string; path: string }>();
-
 // Process results and store in map
 results.forEach((result, index) => {
-  const name = depNames[index];
   if (result.status === "fulfilled") {
-    const data = result.value as { name: string; version: string; path: string };
-    crateVersions.set(data.name, { version: data.version, path: data.path });
-    console.log(`${data.name}: ${data.version} (${data.path})`);
+    const data = result.value as { name: string; version: string };
+    const remote = crates.get(data.name);
+    if (!remote) return;
+    crates.set(data.name, { ...remote, remoteVersion: data.version });
+    console.log(data);
   } else {
     console.error(`\nError fetching version for ${name}:`, result.reason);
-    crateVersions.set(name, { version: "unknown", path: workspaceDeps[name].path });
   }
 });
 
 console.log(`\n`);
+let pushToMainBranch = false;
 await Promise.all(
-  depNames.map(async (name) => {
-    const crateInfo = crateVersions.get(name);
-    if (crateInfo) {
-      const version = `${name}@${crateInfo.version}`;
-      const manifestPath = `${rootPath}${crateInfo.path}`;
-      const cmd = `cd ${manifestPath} && release-plz set-version ${version}`;
-      console.log(cmd);
-      // await $`${cmd}`;
-    }
+  Array.from(crates).map(async ([name, value]) => {
+    if (value.localVersion === value.remoteVersion) return;
+    pushToMainBranch = true;
+    const version = `${name}@${value.remoteVersion}`;
+    const manifestPath = `${rootPath}${value.path}`;
+    const relPlz = `release-plz set-version ${version}`;
+    console.log(relPlz);
+    await $`cd ${manifestPath} && ${relPlz}`;
   })
 );
+
+if (pushToMainBranch) {
+  await $`cd ${rootPath}`;
+  await $`git add --all`;
+  await $`git commit -m "chore(skip): fix release-plz version"`;
+}
+
+console.log("Fix done. Please force push to main branch\ngit push origin [your-branch-name]:main -f");
