18f1d25a94283f2369ee2029351bc3bde61dd9ef
jakzha <jake.zhang@kindredgroup.com>
2025-02-01 21:25:41 +1100

    chore(skip): clear all obel_reflect_derive source files

--- engine/core/reflect/derive/src/meta.rs
+++
@@ -1,7 +0,0 @@
-use obel_reflect_utils::ObelManifest;
-use syn::Path;
-
-/// Returns the correct path for `bevy_reflect`.
-pub(crate) fn get_bevy_reflect_path() -> Path {
-    ObelManifest::shared().get_path("bevy_reflect")
-}
